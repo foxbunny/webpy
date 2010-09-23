a5bf5a40c4b3e0ce601cb861e6e79a6f456eba61
Branko Vukelic <studio@brankovukelic.com>
2010-09-23 19:44:50 +0200

    Added real validator instances to validators module.

--- web/contrib/validators.py
+++ web/contrib/validators.py
@@ -9,6 +9,8 @@ http://code.djangoproject.com/browser/django/trunk/django/core/validators.py?rev
 """
 
 import re
+from gettext import ugettext as _
+
 from web import form
 
 _datere = r'(19|2\d)\d{2}-((?:0?[1-9])|(?:1[0-2]))-((?:0?[1-9])|(?:[12][0-9])|(?:3[0-1]))'
@@ -30,8 +32,31 @@ url_re = re.compile(r'^https?://\S+$')
 zip_code_re = re.compile(r'^\d{5}$')
 zip4_code_re = re.compile(r'^\d{5}-\d{4}$')
 
-# TODO: Make true form validators using web.form, with validation error
-#       messages.
-# TODO: Make validation error messages translatable using ugettext.
+alphanum = form.Validator(_('Please use only letters and numbers.'),
+                          alnum_re.match)
+alphanum_path = form.Validator(_('Please enter a valid relative path.'),
+                               alnumurl_re.match)
+ansi_date = form.Validator(_('Please enter a valid ANSI date.'),
+                           ansi_date_re.match)
+ansi_time = form.Validator(_('Please enter a valid ANSI time.'),
+                           ansi_time_re.match)
+ansi_datetime_re = form.Validator(_('Please enter valid ANSI date and time.'),
+                                  ansi_datetime_re.match)
+email = form.Validator(_('Please enter a valid e-mail.'),
+                       email_re.match)
+integer = form.Validator(_('Please enter an integer value.'),
+                         integer_re.match)
+ip4 = form.Validator(_('Please enter a valid IP address.'),
+                     ip4_re.match)
+phone = form.Validator(_('Please enter a valid phone number.'),
+                       phone_re.match)
+slug = form.Validator(_('Please enter a valid slug.'),
+                      slug_re.match)
+url = form.Validator(_('Please enter a valid URL.'),
+                        url_re.match)
+zip_code = form.Validator(_('Please enter a valid United States ZIP code.'),
+                          zip_code_re.match)
+zip4_code = form.Validator(_('Please enter a valid United States ZIP+4 code.'),
+                           zip4_code_re.match)
 
 
