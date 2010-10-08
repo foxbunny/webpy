31bc438d6e5950fe202e27044dcc426c528ef534
Branko Vukelic <studio@brankovukelic.com>
2010-10-08 19:45:04 +0200

    Rebranded setup.py so it doesn't conflict with web.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@
 
 from distutils.core import setup
 
-setup(name='web.py',
-      version='0.34',
+setup(name='web.py-fox',
+      version='0.34.13fox-1',
       description='web.py: makes web apps',
       author='Aaron Swartz',
       author_email='me@aaronsw.com',
-      maintainer='Anand Chitipothu',
-      maintainer_email='anandology@gmail.com',
-      url=' http://webpy.org/',
+      maintainer='Branko Vukelic',
+      maintainer_email='bg.branko@gmail.com',
+      url='http://github.com/foxbunny/webpy',
       packages=['web', 'web.wsgiserver', 'web.contrib'],
-      long_description="Think about the ideal way to write a web app. Write the code to make it happen.",
-      license="Public domain",
+      long_description="Poor man's framework bolted onto anti-framework.",
+      license="BSD",
       platforms=["any"],
      )
